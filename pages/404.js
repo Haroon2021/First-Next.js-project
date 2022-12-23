107bb453813cf8e28911e816a6b699d6214e41d4
haroon <haroon@Dures-MacBook-Air.local>
2022-12-23 00:12:39 +0000

    Lesson 8 complete

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,22 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            // redirect someone to the homepge if 3 seconds gone 
+            router.push('/')
+        },3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>The mr men page you found does not exist</h1>
-            <h2>Below is a link to the hopepage</h2>
+            <h2>Below is a link to the homepage</h2>
             <p><Link href = '/'>Link here</Link></p>
         </div>
      );
